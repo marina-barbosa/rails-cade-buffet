7c0bd51dad955ab6dd4333aca5902abf56f605e3
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-04-29 10:42:28 -0300

    search

--- cade-buffet/app/controllers/home_controller.rb
+++ cade-buffet/app/controllers/home_controller.rb
@@ -1,5 +1,16 @@
 class HomeController < ApplicationController
-    before_action :check_user_owner_has_buffet, if: :user_signed_in?
-    def index
-    end 
+  before_action :check_user_owner_has_buffet, if: :user_signed_in?
+
+  def index
+  end
+
+  def search
+    search_terms = ["%#{params[:q]}%", "%#{params[:q]}%"]
+
+    @buffets = Buffet.includes(:address, events)
+      .where(
+        "trading_name LIKE ? OR addresses.city LIKE ?",
+        *search_terms
+      )
+  end
 end

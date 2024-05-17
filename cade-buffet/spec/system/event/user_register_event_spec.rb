7bc663e3e70e51d85f22b6e6304bca006749703d
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-05-17 01:07:42 -0300

    add readme e screenshot

--- cade-buffet/spec/system/event/user_register_event_spec.rb
+++ cade-buffet/spec/system/event/user_register_event_spec.rb
@@ -2,28 +2,31 @@ require "rails_helper"
 
 RSpec.describe "Cadastro de Evento", type: :system do
   before do
-    @user = User.create!(name: "Hortencia", cpf: "20202020202", email: "hortencia@email.com", password: "123456", owner: true)
-    @buffet = Buffet.create!(user: @user, commercial_name: "Buffet Original", legal_name: "Buffet Original Ltda", cnpj: "123456789000124", email: "original@buffet.com", phone: "13 3262 1234", description: "Buffet original com pratos tradicionais.")
-    @event = Event.new(name: "Festa de 15 Anos", min_people: 50, max_people: 100, default_event_duration_minutes: 120, alcoholic_drinks: true, decoration: true, parking_service: true, valet: true, exclusive_buffet_location: true, buffet: @buffet)
+    @buffet = Buffet.create!(commercial_name: "Delícias da Terra", legal_name: "Delícias da Terra Buffet e Eventos Ltda.", cnpj: "12345678901234", email: "deliciasdaterra@example.com", phone: "11987654321", description: "Especializado em culinária regional brasileira.")
+    @user = User.create!(email: "hortency@email.com", password: '123456', password_confirmation: '123456', name: "Hortência Flores", cpf: "12345678901", owner: true, buffet_id: 1)
+    # @price = Price.create!(base_price_weekdays: 350000, additional_person_price_weekdays: 30000, extra_event_hour_price_weekdays: 25000, base_price_weekend: 500000, additional_person_price_weekend: 40000, extra_event_hour_price_weekend: 30000)
+    # @event = Event.new(name: "Noite de Mistério", description: "Uma experiência intrigante com jantar temático, mistério e entretenimento ao vivo.", min_people: 40, max_people: 60, default_event_duration_minutes: 300, alcoholic_drinks: true, decoration: true, parking_service: false, valet: false, exclusive_buffet_location: true, price_id: 1, buffet_id: 1)
+
   end
 
-  it "cadastro de evento pelo proprietário do buffet" do
+  it "cadastro de evento pelo dono do buffet" do
     login_as @user
     visit new_event_path
 
-    fill_in "Name", with: @event.name
-    fill_in "Min People", with: @event.min_people
-    fill_in "Max People", with: @event.max_people
-    fill_in "Default Event Duration Minutes", with: @event.default_event_duration_minutes
-    check "Alcoholic Drinks"
+    fill_in "Name", with: "Noite de Mistério"
+    fill_in "Min people", with: 40
+    fill_in "Max people", with: 60
+    fill_in "Default event duration minutes", with: 300
+    check "Alcoholic drinks"
     check "Decoration"
-    check "Parking Service"
+    check "Parking service"
     check "Valet"
-    check "Exclusive Buffet Location"
+    check "Exclusive buffet location"
     click_on "Salvar"
 
-    expect(current_path).to eq event_path(@event)
-    expect(page).to have_content @event.name
+    expect(page).to have_content "TESTE"
+    # expect(current_path).to eq buffet_path(@buffet)
+    expect(page).to have_content "Noite de Mistério"
     expect(page).to have_content "Evento cadastrado com sucesso."
   end
 end
--- cade-buffet/spec/system/event/user_view_events_spec.rb
+++ cade-buffet/spec/system/event/user_view_events_spec.rb
@@ -2,24 +2,35 @@ require "rails_helper"
 
 RSpec.describe "Visualização de Detalhes de Evento", type: :system do
   before do
-    @user = User.create!(name: "Hortencia", cpf: "20202020202", email: "hortencia@email.com", password: "123456", owner: true)
-    @buffet = Buffet.create!(user: @user, commercial_name: "Buffet Original", legal_name: "Buffet Original Ltda", cnpj: "123456789000124", email: "original@buffet.com", phone: "13 3262 1234", description: "Buffet original com pratos tradicionais.")
-    @event = Event.create!(buffet: @buffet, name: "Festa de 15 Anos", min_people: 50, max_people: 100, default_event_duration_minutes: 120, alcoholic_drinks: true, decoration: true, parking_service: true, valet: true, exclusive_buffet_location: true)
+    @buffet = Buffet.create!(commercial_name: "Delícias da Terra", legal_name: "Delícias da Terra Buffet e Eventos Ltda.", cnpj: "12345678901234", email: "deliciasdaterra@example.com", phone: "11987654321", description: "Especializado em culinária regional brasileira.")
+    @user = User.create!(email: "hortency@email.com", password: '123456', password_confirmation: '123456', name: "Hortência Flores", cpf: "12345678901", owner: true, buffet_id: 1)
+    @price = Price.create!(base_price_weekdays: 350000, additional_person_price_weekdays: 30000, extra_event_hour_price_weekdays: 25000, base_price_weekend: 500000, additional_person_price_weekend: 40000, extra_event_hour_price_weekend: 30000)
+    @event = Event.create!(name: "Noite de Mistério", description: "Uma experiência intrigante com jantar temático, mistério e entretenimento ao vivo.", min_people: 40, max_people: 60, default_event_duration_minutes: 300, alcoholic_drinks: true, decoration: true, parking_service: false, valet: false, exclusive_buffet_location: true, price_id: 1, buffet_id: 1)
   end
 
-  it "visualiza detalhes do evento pelo usuário" do
+  it "Dono visualiza detalhes do evento a partir da tela de detalhes do buffet" do
     login_as @user
-    visit event_path(@event)
+    visit buffet_path(@buffet)
 
     expect(page).to have_content @event.name
     expect(page).to have_content @event.description
     expect(page).to have_content "Min. de Pessoas: #{@event.min_people}"
     expect(page).to have_content "Máx. de Pessoas: #{@event.max_people}"
-    expect(page).to have_content "Duração Padrão: #{@event.default_event_duration_minutes / 60} horas (#{@event.default_event_duration_minutes} minutos)"
+    expect(page).to have_content "Duração Padrão:"
+    expect(page).to have_content "#{@event.default_event_duration_minutes / 60} horas (#{@event.default_event_duration_minutes} minutos)"
+    expect(page).to have_content "Duração Padrão:\n#{@event.default_event_duration_minutes / 60} horas (#{@event.default_event_duration_minutes} minutos)"
     expect(page).to have_content "Bebidas Alcoólicas: Sim"
     expect(page).to have_content "Decoração: Sim"
-    expect(page).to have_content "Serviço de Estacionamento: Sim"
-    expect(page).to have_content "Serviço de Valet: Sim"
+    expect(page).to have_content "Serviço de Estacionamento: Não"
+    expect(page).to have_content "Serviço de Valet: Não"
     expect(page).to have_content "Local Exclusivo para Buffet: Sim"
   end
 end
+
+# fazer depois
+# Cliente visualiza detalhes do evento a partir da tela de detalhes do buffet
+# Visitante visualiza detalhes do evento a partir da tela de detalhes do buffet
+
+# Dono visualiza detalhes do evento a partir da tela de detalhes do evento
+# Cliente visualiza detalhes do evento a partir da tela de detalhes do evento
+# Visitante visualiza detalhes do evento a partir da tela de detalhes do evento
